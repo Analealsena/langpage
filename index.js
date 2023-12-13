aeb0a0b3c905b0dd0e58b0367ebda828e602b51b
Carol <lealanah9@gmail.com>
2023-12-13 18:03:32 -0300

    versão um

---
+++ index.js
@@ -0,0 +1,19 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("Leonardo")
+var Bruna = window.document.getElementById("Bruna")
+var Samantha = window.document.getElementById("Samantha")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita(){
+    Leonardo.style= "display:none"
+    Samantha.style= "display:flex"
+    setaDireita.style= "display:none"
+    setaEsquerda.style= "display:flex; margin-top:55px" 
+}
+
+function RolarParaEsquerda(){
+    Leonardo.style= "display:flex"
+    Samantha.style= "display:none"
+    setaDireita.style="display:flex; margin-top:55px"
+    setaEsquerda.style="display:none"
+}
